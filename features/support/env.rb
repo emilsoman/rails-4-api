e49661fcab544773657a762cb3b7ca1ee3a782fb
Emil Soman <emil.soman@gmail.com>
2013-08-16 16:42:13 +0530

    Run cucumber tests
    1) Fix minitest autorun for `bundle exec rake`
    2) Output cucumber report to ./features.html

--- features/support/env.rb
+++ features/support/env.rb
@@ -37,6 +37,14 @@ rescue NameError
   raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
 end
 
+Before do
+  DatabaseCleaner.start
+end
+
+After do |scenario|
+  DatabaseCleaner.clean
+end
+
 # You may also want to configure DatabaseCleaner to use different strategies for certain features and scenarios.
 # See the DatabaseCleaner documentation for details. Example:
 #
